8616394d0bb4b8072ea1fa9164a3c6b58ee43b1c
Bruno Prado <brunocesarprado@gmail.com>
2016-04-22 15:42:03 -0300

    Pastas

--- Main/RocketDynEq.m
+++ Main/RocketDynEq.m
@@ -17,28 +17,19 @@ rh = rh0*exp(-h/H0);            % Atmospheric density exponential model
 D = 1/2 * rh*v^2 * A * Cd;      % Drag force
 
 % Rocket starts the gravity turn when h = hgr_turn
-if h <= hgr_turn && h>0 % Vertical flight
+if h <= hgr_turn % Vertical flight
     dv_dt  = T/m - D/m - g;
     dgm_dt = 0;
     dx_dt  = 0;
     dh_dt  = v;
     dvG_dt = -g;
-else if h <= 0 %if is under rocket level
-    dv_dt  = 0;
-    dgm_dt = 0;
-    dx_dt  = 0;
-    dh_dt  = v;
-    dvG_dt = -g;   
-else
-    % Gravity turn
+else                                  % Gravity turn
     dv_dt  = T/m - D/m - g*sin(gm);
     dgm_dt = -1/v*(g - v^2/(Re + h))*cos(gm);
     dx_dt  = Re/(Re + h)*v*cos(gm) + 463*sin(gm);
     dh_dt  = v*sin(gm) + 463*cos(gm); % Adding earth's rotation speed
     dvG_dt = -g*sin(gm);              % Gravity loss rate [m/s^2]
-    end
 end
-
     dvD_dt = -D/m;           % Drag loss rate [m/s^2]
 dfdt = [ dv_dt,dgm_dt, dx_dt,dh_dt, dvD_dt, dvG_dt]';
 return
